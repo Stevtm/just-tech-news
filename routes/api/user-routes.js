4dc78ef90102b3d5866a5522db8d9a743d84eb23
Stevtm <steventmaddison@outlook.com>
2021-05-11 20:09:41 -0400

    add login route with password validation

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,30 @@ router.post("/", (req, res) => {
 		});
 });
 
+// POST /api/users/login
+router.post("/login", (req, res) => {
+	User.findOne({
+		where: {
+			email: req.body.email,
+		},
+	}).then((dbUserData) => {
+		if (!dbUserData) {
+			res
+				.status(400)
+				.json({ message: "No user with that email address exists!" });
+			return;
+		}
+
+		const validPassword = dbUserData.checkPassword(req.body.password);
+		if (!validPassword) {
+			res.status(400).json({ message: "Incorrect password!" });
+			return;
+		}
+
+		res.json({ user: dbUserData, message: "You are now logged in!" });
+	});
+});
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
 	// if req.body has exact key/value pair to match the model, req.body can be used
