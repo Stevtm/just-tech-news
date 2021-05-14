eb3979d12787389c5df910ba6e76d08143b65b87
Stevtm <steventmaddison@outlook.com>
2021-05-13 22:48:44 -0400

    add GET and DELETE routes for Comment model

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+	Comment.findAll({})
+		.then((dbCommentData) => {
+			res.json(dbCommentData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
 
 router.post("/", (req, res) => {
 	const { comment_text, user_id, post_id } = req.body;
@@ -20,6 +29,24 @@ router.post("/", (req, res) => {
 		});
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+	Comment.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbCommentData) => {
+			if (!dbCommentData) {
+				res.status(404).json({ message: "No comment with that id was found!" });
+				return;
+			}
+
+			res.status(200).json(dbCommentData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
 
 module.exports = router;
