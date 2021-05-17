a3238643924b39478324c9d00ae249f08069131b
Stevtm <steventmaddison@outlook.com>
2021-05-17 10:42:48 -0400

    create homepage using handlebars and request data

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+
+// require express-handlebars
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 // initialize app
 const app = express();
@@ -11,6 +15,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// set up express handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
