329f3ba80f206aa73aa9bb00ceeae2339af8c9c7
Stevtm <steventmaddison@outlook.com>
2021-05-17 10:02:29 -0400

    add pre-built stylesheet and set up static route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 // set up express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
